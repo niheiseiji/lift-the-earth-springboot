256f8ecf649db68e6877c716c0226bd3437ed02d
nihei.seiji <nihei.seiji.office@gmail.com>
2025-04-19 15:29:46 +0900

    first

---
+++ src/test/java/com/lifttheearth/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifttheearth.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
